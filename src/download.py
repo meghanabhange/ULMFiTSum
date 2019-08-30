6edc852a1e71a8cd0a75b41fea18f290848fbaa3
meghanabhange <meghana@verloop.io>
2019-08-30 20:16:33 +0530

    Add downloader for indosum and downloading LM

--- src/download.py
+++ src/download.py
@@ -3,14 +3,16 @@ from pathlib import Path
 import fire
 
 
-def get_dataset(dataset="indosum"):
-    return dataset
+def get_dataset(data_path=Path("./data"), dataset="indosum", pretrained="indo_lm"):
+    return data_path, dataset, pretrained
 
 
 def main():
-    data_path = Path("./data")
-    dataset = fire.Fire(get_dataset)
-    data_handler = DataHandler(data_path, "indosum")
+    data_path, dataset, pretrained= fire.Fire(get_dataset)
+    data_handler = DataHandler(data_path)
+    data_handler.downloader(dataset)
+    if pretrained=="indo_lm":
+        data_handler.downloader("indo_lm")
 
 
 if __name__ == "__main__":
--- src/utils.py
+++ src/utils.py
@@ -12,58 +12,58 @@ logging.getLogger().setLevel(logging.INFO)
 
 
 class DataHandler:
-    def __init__(self, data_path, dataset_name):
+    def __init__(self, data_path):
         self.data_path = data_path
-        self.dataset_name = dataset_name
         logging.info(f"Data Handler Class Created - {data_path}")
-        if dataset_name.lower() == "indosum":
-            self.download_indosum()
 
-    def download_indosum(self):
+    def downloader(self, download_type):
         """
-        Downaloads Indosum in data_path/indosum.
+        Downaloads download_type in data_path/download_type.
         """
-        logging.info("Downloading Indosum")
-        if not self.check_data_exists(self.dataset_name):
-            (self.data_path / self.dataset_name).mkdir(parents=True, exist_ok=True)
+        logging.info(f"Downloading {download_type}")
+        if not self.check_data_exists(download_type):
+            (self.data_path / download_type).mkdir(parents=True, exist_ok=True)
         else:
             logging.info("Folder already exists")
-        if not (self.data_path / self.dataset_name / "indosum.tar.gz").exists():
+        if not (self.data_path / download_type / f"{download_type}.tar.gz").exists():
+            google_drive_link = {
+                "indosum": "https://docs.google.com/uc?export=download&id=1OgYbPfXFAv3TbwP1Qcwt_CC9cVWSJaco",
+                "indo_lm": "link_to_pretrained_gooogle_lm",
+            }
             output_download = subprocess.check_output(
                 [
                     "wget",
                     "--no-check-certificate",
-                    "https://docs.google.com/uc?export=download&id=1OgYbPfXFAv3TbwP1Qcwt_CC9cVWSJaco",
+                    google_drive_link[download_type],
                     "-O",
-                    f"{self.data_path/self.dataset_name/'indosum.tar.gz'}",
+                    f"{self.data_path/download_type/f'{download_type}.tar.gz'}",
                 ]
             )
             logging.info(f"{output_download}")
-        if not (self.data_path / self.dataset_name / "indosum").exists():
+        if not (self.data_path / download_type / f"{download_type}").exists():
             ouput_unzip = subprocess.check_output(
                 [
                     "tar",
                     "xvzf",
-                    f"{self.data_path/self.dataset_name/'indosum.tar.gz'}",
+                    f"{self.data_path/download_type/f'{download_type}.tar.gz'}",
                     "-C",
-                    f"{self.data_path/self.dataset_name}",
+                    f"{self.data_path/download_type}",
                 ]
             )
             logging.info(f"{ouput_unzip}")
-        logging.info(f"{self.dataset_name} Downloaded")
+        logging.info(f"{download_type} Downloaded")
 
-    def check_data_exists(self, name_of_dataset):
+    def check_data_exists(self, folder_name):
         """
         Checks the data folders existence
 
         Arguments:
-            name_of_dataset {[str]} -- [Name of the dataset to be downloaded]
+            folder_name {[str]} -- [Name of the folder to be downloaded]
 
         Returns:
             [bool] -- [True is the data folder exists]
         """
-        logging.info(f"Checking for : {name_of_dataset}")
-        exists = (self.data_path / name_of_dataset).exists()
-        logging.info(f"{name_of_dataset} Folder Existence status : {exists}")
+        logging.info(f"Checking for : {folder_name}")
+        exists = (self.data_path / folder_name).exists()
+        logging.info(f"{folder_name} Folder Existence status : {exists}")
         return exists
-
