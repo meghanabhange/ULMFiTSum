01c8ce202c36143ee06c1009d242c51559bfb4bd
meghanabhange <meghana@verloop.io>
2019-09-08 15:57:10 +0530

    Parameterize decouple loading data_lm

--- src/wiki_ulmfit.py
+++ src/wiki_ulmfit.py
@@ -12,28 +12,29 @@ from src.utils import LangTokenizer
 logger = logging.getLogger()
 
 
+def load_data_file(
+    path, fname_pkl="data_save.pkl", fname_text_file="id_wiki.txt", lang="id"
+):
+    tokenizer = Tokenizer(tok_func=LangTokenizer, lang=lang)
+    if (path / fname_pkl).exists():
+        data_file = load_data(path)
+    else:
+        data_file = TextLMDataBunch.from_csv(
+            path,
+            fname_text_file,
+            tokenizer=tokenizer,
+            bs=48,
+            text_cols=0,
+            label_cols=None,
+        )
+        data_file.save()
+
+    return data_file
+
+
 class WikiTrainer:
     def __init__(self):
         logger.info("I work--for now")
-
-    def load_data_lm(self):
-        """
-        Loads data_lm for wikipedia data.
-        """
-        tokenizer = Tokenizer(tok_func=LangTokenizer, lang=self.lang)
-        if (self.path / "data_save.pkl").exists():
-            data_lm = load_data(self.path)
-        else:
-            data_lm = TextLMDataBunch.from_csv(
-                self.path,
-                f"{self.lang}_wiki.txt",
-                tokenizer=tokenizer,
-                bs=48,
-                text_cols=0,
-                label_cols=None,
-            )
-            data_lm.save()
-        self.data_lm = data_lm
 
     def train(self, lr=5e-4, n_epocs=1, fname_out="idwiki-latest"):
         learn = language_model_learner(self.data_lm, AWD_LSTM, drop_mult=0.3)
@@ -48,7 +49,7 @@ class WikiTrainer:
         path="./data",
         encoder=True,
         lang="id",
-        **kwargs
+        **kwargs,
     ):
         """
         Predicts the next_tok(int) after a given seed string start
@@ -66,7 +67,12 @@ class WikiTrainer:
        """
         self.path = Path(path)
         self.lang = lang
-        self.load_data_lm()
+        self.data_lm = load_data_file(
+            path=self.path,
+            fname_pkl="data_save.pkl",
+            fname_text_file=f"{self.lang}_wiki.txt",
+            lang=self.lang,
+        )
         learn = language_model_learner(self.data_lm, AWD_LSTM, drop_mult=0.3)
         if encoder:
             learn.load_encoder(model_name)
